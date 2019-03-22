cdcbd87380b0333e4b2d243a437c17b3b2fee73c
Alex Ryberg <alex.ryberg@hotmail.com>
2019-03-22 13:32:07 +0100

    add webRequest interceptor and remove activeTab permission

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,1 +1,12 @@
 console.log('hello from background');
+
+console.log(chrome.webRequest);
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  details => {
+    console.log('onBeforeSendHeaders', details);
+  },
+  {
+    urls: ['https://*.google.com/search*'],
+  },
+);
