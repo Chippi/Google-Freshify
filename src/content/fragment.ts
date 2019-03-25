e3d54ea9855ca788875fca8e75e9347ace0718ed
Alex Ryberg <alex.ryberg@hotmail.com>
2019-03-25 17:19:05 +0100

    add slider labels & update slider ui

--- src/content/fragment.ts
+++ src/content/fragment.ts
@@ -10,8 +10,11 @@ function tag(tagName: string, style: string = '', children?: HTMLElement[]) {
   return el;
 }
 
-const LABEL = (style: string, children?: HTMLElement[]) => {
-  return tag('label', style, children);
+const createPTag = (title: string) => {
+  const pTag = tag('p');
+  pTag.innerText = title;
+
+  return pTag;
 };
 
 const DIV = (style: string, children?: HTMLElement[]) => tag('div', style, children);
@@ -35,10 +38,24 @@ function createFragment(model, onChange: (val: number, e?: Event) => void) {
       onChange(val, e);
     };
 
-    return LABEL('', [range]);
+    return range;
   };
 
-  return DIV('', [rangePartial(), DIV('')]);
+  return DIV('', [rangePartial(), createLabels()]);
 }
+
+const createLabels = () => {
+  const wrapper = tag('div');
+  wrapper.classList.add('yolo__labels');
+
+  wrapper.appendChild(createPTag('1 hour'));
+  wrapper.appendChild(createPTag('1 day'));
+  wrapper.appendChild(createPTag('1 week'));
+  wrapper.appendChild(createPTag('1 month'));
+  wrapper.appendChild(createPTag('1 year'));
+  wrapper.appendChild(createPTag('Any time'));
+
+  return wrapper;
+};
 
 export { createFragment };
