71919510d668ee8ffb6374e640252f71f922b8e6
Alex Ryberg <alex.ryberg@hotmail.com>
2019-06-28 14:46:45 +0200

    feat(domains): update domains list

--- src/domains.js
+++ src/domains.js
@@ -1,4 +1,4 @@
 // https://infogalactic.com/info/List_of_Google_domains
-const googleDomains = ['com', 'se', 'uk', 'fi', 'no', 'dk', 'fr', 'de'];
+const googleDomains = ['com', 'se', 'uk', 'fi', 'no', 'dk'];
 
 module.exports = googleDomains;
