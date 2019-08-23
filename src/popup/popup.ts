c8a2428f3aa359ef37981bad9f99f449b28b0562
Alex Ryberg <alex.ryberg@hotmail.com>
2019-08-23 13:59:52 +0200

    feat(storage): add storage get & save for clearInput values

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,1 +1,21 @@
 import './popup.scss';
+
+const clearInput = document.querySelector<HTMLInputElement>('#clearInput');
+
+chrome.storage.sync.get('saveInMinutes', obj => {
+  const value = obj && obj.saveInMinutes;
+
+  if (value) {
+    clearInput.value = value;
+  }
+
+  listenForInputChanges();
+});
+
+const listenForInputChanges = () => {
+  clearInput.addEventListener('change', event => {
+    const value = clearInput.value;
+
+    chrome.storage.sync.set({ saveInMinutes: value });
+  });
+};
